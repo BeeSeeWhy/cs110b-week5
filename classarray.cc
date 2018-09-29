a5e6c6705d8e7adffe37ed875b9f962243467c0b
bcruz13 <bcruz13@hills.ccsf.edu>
2018-09-28 22:31:33 -0700

    finishing touches by group

--- classarray.cc
+++ classarray.cc
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 #include <stdlib.h>
+#include <string>
 using namespace std;
 
 //global variable so we can access by reference
@@ -24,20 +24,17 @@ public:
 //this concatenates so we can use
 // addressBook[i].name() for sorting
 string Person::name() {
-	string name = firstName + " " + lastName;
-
+	string name = lastName + ", " + firstName;
 	return name;
 }
 
 //protoypes since they call each other
-void addNewEntry(Person array[], int &);
-void deleteEntry(Person array[], int &);
-void editEntry(Person array[], int &);
-void hardCodePeople(Person array[], int &);
-void menu(Person array[], int &);
-void printAddressBook(Person array[], int);
-void search(Person array[], int);
-void sortArray(Person array[], int);
+void addNewEntry(Person[], int &);
+void hardCodePeople(Person[], int &);
+void menu(Person[], int &);
+void printAddressBook(Person[], int);
+void search(Person[], int &);
+void sortArray(Person[], int);
 
 int main() {
 	const int ARRAY_SIZE = 100;
@@ -49,273 +46,109 @@ int main() {
 	//hard code people in address book
 	hardCodePeople(addressBook, numberOfPeople);
 
-
 	//call menu
 	menu(addressBook, numberOfPeople);
 
-
-
 	return 0;
+
 }
-
-
-
 //adding an entry
 void addNewEntry(Person persons[], int &numberOfPeople) {
-  int i = numberOfPeople;
+	cout << endl;
+	int i = numberOfPeople;
+	cin.ignore();
+	cout << "Enter last name : ";
+	getline(cin, persons[i].lastName, '\n');
 
-  cin.ignore();
-  cout << "Enter last name : ";
-  getline(cin, persons[i].lastName, '\n');
+	cout << "Enter first name: ";
+	getline(cin, persons[i].firstName, '\n');
 
-  cout << "Enter first name: ";
-  getline(cin, persons[i].firstName, '\n');
+	cout << "Enter telephone number: ";
+	getline(cin, persons[i].telephoneNumber, '\n');
 
-  cout << "Enter telephone number: ";
-  getline(cin, persons[i].telephoneNumber, '\n');
+	cout << "Enter street address: ";
+	getline(cin, persons[i].streetAddress, '\n');
 
-  cout << "Enter street address: ";
-  getline(cin, persons[i].streetAddress, '\n');
+	cout << "Enter email: ";
+	getline(cin, persons[i].email, '\n');
+	cout << endl;
+	cout << endl;
 
-  cout << "Enter email: ";
-  getline(cin, persons[i].email, '\n');
+	//increment so we know how many elements
+	//are used in array
+	numberOfPeople++;
 
-  cout << endl;
-  cout << endl;
-
-  //increment so we know how many elements
-  //are used in array
-  numberOfPeople++;
-
-  printAddressBook(persons, numberOfPeople);
-  menu(persons, numberOfPeople);
-}
-
-// Search and delete an entry
-void deleteEntry(Person persons[], int &numberOfPeople) {
-  const int ARRAY_SIZE = 100;
-  Person addressBook[ARRAY_SIZE];
-  int userSearch;
-  char deleteChoice;
-
-  string fname;
-  string lname;
-  cout << endl;
-	cout << "1. Search First Name" << endl;
-	cout << "2. Search Last Name" << endl;
-	cout << "Enter your choice (1-2): ";
-	cin >> userSearch;
-
-  switch (userSearch) {
-    case 1: cout << "First Name: ";
-            cin >> fname;
-
-            for (int i = 0; i < numberOfPeople; i++) {
-              if (fname.compare(persons[i].firstName) == 0 ){
-                cout << "Are you sure that you want to edit this entry? Y/N" << endl;
-                cout << persons[i].name() << endl;
-                cin >> deleteChoice;
-                if (deleteChoice == 'Y' || 'y') {
-                  cin.ignore();
-
-                  Person* persons[i];
-                  delete persons[i];
-
-                  numberOfPeople--;
-                  cout << "The entry has been deleted " << endl;
-                  cout << endl;
-                }
-              else
-                  cout << "Start over from the main menu" << endl;
-              }
-            }
-            cin.ignore();
-            break;
-    case 2: cout << "Last Name: ";
-            cin >> lname;
-
-            for (int i = 0; i < numberOfPeople; i++) {
-              if (lname.compare(persons[i].lastName) == 0 ){
-                cout << "Are you sure that you want to edit this entry? Y/N" << endl;
-                cout << persons[i].name() << endl;
-                cin >> deleteChoice;
-                if (deleteChoice == 'Y' || 'y') {
-                  cin.ignore();
-
-                  Person* persons[i];
-                  delete persons[i];
-
-                  numberOfPeople--;
-                  cout << "The entry has been deleted " << endl;
-                  cout << endl;
-                }
-              }
-              else
-                cout << "Start over from the main menu" << endl;
-            }
-            break;
-
-     }
-}
-
-// Search and edit an entry
-void editEntry(Person persons[], int &numberOfPeople) {
-
-  const int ARRAY_SIZE = 100;
-  Person addressBook[ARRAY_SIZE];
-  int userSearch;
-  char deleteChoice;
-
-  string fname;
-  string lname;
-
-  cout << endl;
-	cout << "1. Search First Name" << endl;
-	cout << "2. Search Last Name" << endl;
-	cout << "Enter your choice (1-2): ";
-	cin >> userSearch;
-
-  switch (userSearch) {
-    case 1 :  cout << "First Name: ";
-              cin >> fname;
-              for (int i = 0; i < numberOfPeople; i++) {
-                if (fname.compare(persons[i].firstName) == 0 ){
-                  cout << "Are you sure that you want to edit this entry? Y/N" << endl;
-                  cout << persons[i].name() << endl;
-                  cin >> deleteChoice;
-                  if (deleteChoice == 'Y' || 'y') {
-                    cin.ignore();
-
-                    cout << "Enter last name : ";
-                    getline(cin, addressBook[i].lastName);
-
-                    cout << "Enter first name: ";
-                    getline(cin, addressBook[i].firstName);
-
-                    cout << "Enter telephone number: ";
-                    getline(cin, addressBook[i].telephoneNumber);
-
-                    cout << "Enter street address: ";
-                    getline(cin, addressBook[i].streetAddress);
-
-                    cout << "Enter email: ";
-                    getline(cin, addressBook[i].email);
-                    cout << endl;
-                  }
-                  else
-                    cout << "Start over from the main menu" << endl;
-                }
-              }
-              cin.ignore();
-              break;
-    case 2 :  cout << "Last Name: ";
-              cin >> lname;
-
-              for (int i = 0; i < numberOfPeople; i++) {
-                if (lname.compare(persons[i].lastName) == 0 ) {
-                  cout << "Are you sure that you want to edit this entry? Y/N" << endl;
-                  cout << persons[i].name() << endl;
-                  cin >> deleteChoice;
-                  if (deleteChoice == 'Y' || 'y') {
-                    cin.ignore();
-                    cout << "Enter last name : ";
-                    getline(cin, addressBook[i].lastName);
-
-                    cout << "Enter first name: ";
-                    getline(cin, addressBook[i].firstName);
-
-                    cout << "Enter telephone number: ";
-                    getline(cin, addressBook[i].telephoneNumber);
-
-                    cout << "Enter street address: ";
-                    getline(cin, addressBook[i].streetAddress);
-
-                    cout << "Enter email: ";
-                    getline(cin, addressBook[i].email);
-                    cout << endl;
-                  }
-                  else
-                    cout << "Start over from the main menu" << endl;
-                }
-              }
-
-              cin.ignore();
-              break;
-    }
+	cout << numberOfPeople << endl; //test to make sure increment
+	printAddressBook(persons, numberOfPeople);
+	menu(persons, numberOfPeople);
 }
 
 void hardCodePeople(Person persons[], int &numberOfPeople) {
-  persons[0].lastName = "Sioux";
-  persons[0].firstName = "Siouxsie";
-  persons[0].telephoneNumber = "702-555-5555";
-  persons[0].streetAddress = "3570 S Las Vegas Blvd, Las Vegas, NV 89109";
-  persons[0].email = "siouxsie@sioux.com";
-  numberOfPeople++;
+	persons[0].lastName = "Sioux";
+	persons[0].firstName = "Siouxsie";
+	persons[0].telephoneNumber = "702-555-5555";
+	persons[0].streetAddress = "3570 S Las Vegas Blvd, Las Vegas, NV 89109";
+	persons[0].email = "siouxsie@sioux.com";
 
-  persons[1].lastName = "Addams";
-  persons[1].firstName = "Morticia";
-  persons[1].telephoneNumber = "415-666-1313";
-  persons[1].streetAddress = "13 Funston Street, San Francisco, CA 94129";
-  persons[1].email = "morticia@addams.com";
-  numberOfPeople++;
+	numberOfPeople++;
 
-  persons[2].lastName = "Bates";
-  persons[2].firstName = "Norman";
-  persons[2].telephoneNumber = "323-555-5555";
-  persons[2].streetAddress = "1070 272nd Street, Aldergrove, BC V4W 2P8, Canada";
-  persons[2].email = "motherknowsbest@bates.com";
-  numberOfPeople++;
+	persons[1].lastName = "Addams";
+	persons[1].firstName = "Morticia";
+	persons[1].telephoneNumber = "415-666-1313";
+	persons[1].streetAddress = "13 Funston Street, San Francisco, CA 94129";
+	persons[1].email = "morticia@addams.com";
+
+	numberOfPeople++;
+
+	persons[2].lastName = "Bates";
+	persons[2].firstName = "Norman";
+	persons[2].telephoneNumber = "323-555-5555";
+	persons[2].streetAddress = "1070 272nd Street, Aldergrove, BC V4W 2P8, Canada";
+	persons[2].email = "motherknowsbest@bates.com";
+	numberOfPeople++;
 }
 
 //menu and user choice
 void menu(Person persons[], int &numberOfPeople) {
- int choice;
- do {
-   cout << "1. Add New Entry" << endl;
-   cout << "2. Delete An Entry" << endl;
-   cout << "3. Edit An Entry" << endl;
-   cout << "4. List All Entries" << endl;
-   cout << "5. Search For An Entry" << endl;
-   cout << "6. Quit Program" << endl;
-   cout << "Enter your choice (1-6): " << endl;
-   cin >> choice;
+	int choice;
+	do {
+		cout << "1. Add New Entry" << endl;
+		cout << "2. List All Entries" << endl;
+		cout << "3. Search For An Entry" << endl;
+		cout << "4. Quit Program" << endl;
+		cout << "Enter your choice (1-4): ";
+		cin >> choice;
 
- } while (!(choice > 0 && choice < 7));
-
- switch (choice) {
-   case 1 :  addNewEntry(persons, numberOfPeople);
-             break;
-   case 2 :  deleteEntry(persons, numberOfPeople);
-             break;
-   case 3 :  editEntry(persons, numberOfPeople);
-             break;
-   case 4 :  printAddressBook(persons, numberOfPeople);
-             break;
-   case 5 :  search(persons, numberOfPeople);
-             break;
-   case 6 :  exit(0);
- }
+	} while (!(choice > 0 && choice < 7));
+	switch (choice) {
+	case 1:  addNewEntry(persons, numberOfPeople);
+		break;
+	case 2:  printAddressBook(persons, numberOfPeople);
+		break;
+	case 3:  search(persons, numberOfPeople);
+		break;
+	case 4:  exit(0);
+	}
 }
 
 void printAddressBook(Person persons[], int numberOfPeople) {
-  //sort before print
-  sortArray(persons, numberOfPeople);
+	//sort before print
+	sortArray(persons, numberOfPeople);
 
-  //test to make sure working
-  for(int i = 0; i < numberOfPeople; i++) {
-    cout << setw(5) << left << i + 1;
-    cout << persons[i].name() << endl;
-    cout << setw(5) << " " << persons[i].telephoneNumber << endl;
-    cout << setw(5) << " " << persons[i].streetAddress << endl;
-    cout << setw(5) << " " << persons[i].email << endl;
-    cout << endl;
-  }
-
-  menu(persons, numberOfPeople);
+	//test to make sure working
+	for (int i = 0; i < numberOfPeople; i++) {
+		cout << setw(5) << left << i + 1;
+		cout << persons[i].name() << endl;
+		cout << setw(5) << ' ' << persons[i].telephoneNumber << endl;
+		cout << setw(5) << ' ' << persons[i].streetAddress << endl;
+		cout << setw(5) << ' ' << persons[i].email << endl;
+		cout << endl;
+		}
+	menu(persons, numberOfPeople);
 }
 
 //Searching the address book
-void search(Person persons[], int numberOfPeople) {
+void search(Person persons[], int &numberOfPeople) {
 	int userSearch;
 	string fname;
 	string lname;
@@ -371,7 +204,7 @@ void search(Person persons[], int numberOfPeople) {
 					}
 					else {
 						cout << "Start over from the main menu" << endl;
-					}
+			  	}
 					break;
 				case 2: // Delete
 					cout << "Are you sure that you want to edit this entry? Y/N" << endl;
@@ -391,7 +224,6 @@ void search(Person persons[], int numberOfPeople) {
 						persons[numberOfPeople].email = "";
 
 						numberOfPeople -= 1;
-
 						cout << "The entry has been deleted " << endl;
 						cout << endl;
 					}
@@ -413,7 +245,6 @@ void search(Person persons[], int numberOfPeople) {
 	case 2:
 		cout << "Last Name: ";
 		cin >> lname;
-
 		for (int i = 0; i < numberOfPeople; i++) {
 			if (lname.compare(persons[i].lastName) == 0) {
 				cout << persons[i].name() << endl;
@@ -486,7 +317,7 @@ void search(Person persons[], int numberOfPeople) {
 					break;
 				default: cout << "Not a valid entry" << endl;
 					break;
-				}
+        }
 			}
 		}
 		cout << endl;
@@ -500,7 +331,6 @@ void search(Person persons[], int numberOfPeople) {
 	menu(persons, numberOfPeople);
 }
 
-//sorting address book alphabetically
 void sortArray(Person persons[], int length) {
 	int i, j;
 	for (i = 0; i < length - 1; i++) {
